ae2b0155a54db4cceecbb7235ab6ded591c768fa
Genie <jeonbg@kakao.com>
2022-07-13 01:58:28 +0900

    Setup : React-Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@emotion/react';
@@ -6,16 +7,28 @@ import { globalStyles } from '@/styles/globalStyles';
 import { theme } from '@/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Summoners War : Battle</title>
       </Head>
-      <ThemeProvider theme={theme}>
-        {globalStyles}
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider theme={theme}>
+            {globalStyles}
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
